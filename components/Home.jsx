d9de04b9da6b0b05f3658331a8eb47372933cd9d
kilisoria <cristiansoria@gmail.com>
2021-07-13 14:39:19 -0300

    typo fix

--- components/Home.jsx
+++ components/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => (
   <div className="hero my-5 text-center" data-testid="hero">
     <Logo testId="hero-logo" />
     <h1 className="mb-4" data-testid="hero-title">
-      Welcome to the Band of Coders Challange!
+      Welcome to the Band of Coders Challenge!
     </h1>
     <p className="lead" data-testid="hero-lead">
       This is an application that demonstrates the use of Nodejs, Express, React, Redux, etc.
